cf7cc3458402c1bd6431c95c62c8039aa0d20766
ashishchauhannn <101080857+ashishchauhannn@users.noreply.github.com>
2024-06-24 22:11:53 +0530

    api updated

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from './config/database.js';
 // const database = require("./config/database")
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 import dotenv from "dotenv";
 import messageRoute from './routes/messageRoute.js';
 import userRoute from './routes/userRoute.js';
@@ -11,12 +12,18 @@ const PORT = process.env.PORT || 8080;
 
 
 // middleware 
-
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
+const corsOption = {
+    origin: 'http://localhost:3000',
+    credentials: true
+};
+app.use(cors(corsOption));
 //
 app.use("/api/v1/user", userRoute)
 app.use("/api/v1/message", messageRoute)
+
 
 
 app.listen(PORT, () => {
